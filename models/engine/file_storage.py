2331d4983b549126a053cb9c9b59a5fd32d35921
luismedinaeng <malf95@hotmail.com>
2020-03-29 17:10:53 -0500

    [Add]Delete method to the file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,3 +55,13 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """deletes a given obj from __objects
+        Args:
+            obj: given object
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
