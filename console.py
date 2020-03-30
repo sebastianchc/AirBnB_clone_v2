9888eb7b928c9efd8e5eef9bf88fe27510f7a86d
sebastianchc <1200@holbertonschool.com>
2020-03-30 01:15:16 -0500

    [Update] Create command improvement

--- console.py
+++ console.py
@@ -43,6 +43,12 @@ class HBNBCommand(cmd.Cmd):
                 raise SyntaxError()
             my_list = line.split(" ")
             obj = eval("{}()".format(my_list[0]))
+            for attributes in range(1, len(my_list)):
+                command = my_list[i]
+                command = command.replace("=", " ")
+                att_val = command.split()
+                att_val[1] = att_val[1].replace("_", " ")
+                setattr(obj, att_val[0], att_val[1])
             obj.save()
             print("{}".format(obj.id))
         except SyntaxError:
