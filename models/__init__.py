8619195d37fdab97b6463b67dfbaf5ef1e61b10d
sebastianchc <1200@holbertonschool.com>
2020-03-30 14:30:52 -0500

    [Task 6] Update Base model class Update State class Update City class Update __init__ Create DBStorage class

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """create a unique FileStorage instance for your application"""
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -8,7 +9,12 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+from os import environ
 
 
-storage = FileStorage()
+storage_value = environ.get("HBNB_TYPE_STORAGE")
+if storage_value == "db":
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete", backref="state")
+
+    @property
+    def cities(self):
+        """return the list of City instances with state_id=State.id
+        """
+        list_cities = []
+        state_cities = models.engine.all(City)
+        for st_city in state_cities.values():
+            if st_city.state_id == self.id:
+                list_cities.append(st_city)
+        return list_cities
