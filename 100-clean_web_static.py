08f5ad9f91da4d7b637fb22f9b0718d83c57325b
sebastianchc <1200@holbertonschool.com>
2020-04-12 21:26:49 -0500

    [Update] Using ; for execute cd and delete

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -57,9 +57,10 @@ def deploy():
 
 def do_clean(number=0):
     number = int(number)
+    path = "/data/web_static/releases"
     if number == 0:
         number = 2
     else:
-        number +=1
-    local("cd versions/")
-    local("ls -t | tail -n +{} | xargs rm -fr".format(number))
+        number += 1
+    local("cd versions ; ls -t | tail -n +{} | xargs rm -fr".format(number))
+    run("cd {} ; ls -t | tail -n +{} | xargs rm -fr".format(path, number))
