8fb448ea4d9c41d9ed393e1a62ab8ce84b819cc2
sebastianchc <1200@holbertonschool.com>
2020-04-14 12:40:07 -0500

    [Fix] fix line location

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,44 +1,44 @@
 # Set up web server for deployment of web static with puppet
-exec { 'Update' :
+exec { 'Update':
      command => '/usr/bin/apt-get -y update'
 }
 
-exec { 'Install' :
+exec { 'Install':
      require => Exec['Update'],
      command => '/usr/bin/apt-get -y install nginx'
 }
 
-exec { 'Test' :
+exec { 'Shared':
      require => Exec['Install'],
-     command => '/bin/mkdir -p /data/web_static/releases/test/'
-}
-
-exec { 'Share' :
-     require => Exec['Test'],
      command => '/bin/mkdir -p /data/web_static/shared/'
 }
 
-exec { 'Content' :
-     require => Exec['Share'],
-     command => '/bin/echo "Holberton" > /data/web_static/releases/test/index.html'
+exec { 'Releases':
+     require => Exec['Shared'],
+     command => '/bin/mkdir -p /data/web_static/releases/test/'
 }
 
-exec { 'Symbolic' :
+exec { 'Content':
+     require => Exec['Releases'],
+     command => '/bin/echo "Holberton School" > /data/web_static/releases/test/index.html'
+}
+
+exec { 'Symbolic':
      require => Exec['Content'],
-     command => '/bin/ln -fs /data/web_static/releases/test/ /data/web_static/current'
+     command => '/bin/ln -sf /data/web_static/releases/test/ /data/web_static/current'
 }
 
-exec { 'Permissions' :
+exec { 'Permissions':
      require => Exec['Symbolic'],
-     command => '/bin/chown -R -R ubuntu:ubuntu /data/'
+     command => '/bin/chown -R ubuntu:ubuntu /data/'
 }
 
-exec { 'Location' :
-     require: => Exec['Permissions'],
-     command => '/bin/sed -i "38i location /hbnb_static {\n\t\talias /data/web_static/current;\n\t}" /etc/nginx/sites-enabled/default'
+exec { 'Location':
+     require     => Exec['Permissions'],
+     command     => '/bin/sed -i "38i location /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n" /etc/nginx/sites-enabled/default'
 }
 
-exec { 'Restart' :
+exec { 'Restart':
      require => Exec['Location'],
      command => '/usr/sbin/service nginx restart'
 }
