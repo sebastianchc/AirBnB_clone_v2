1276da98ffdd82d8a2eaffb72fbddc59c346967a
sebastianchc <1200@holbertonschool.com>
2020-04-22 12:06:49 -0500

    [Update] task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+from models.state import State
+from models import storage
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_session(error):
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    list_states =[]
+    states = models.storage.all(State)
+    for state in states.values():
+        list_states.append(state)
+    return render_template("7-states_list.html", states=list_states)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
