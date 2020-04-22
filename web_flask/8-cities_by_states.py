14213b5893e3f3487febbb346b1bdf92a8b196d9
sebastianchc <1200@holbertonschool.com>
2020-04-22 16:33:22 -0500

    [Update]

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,10 +10,10 @@ def close_session(error):
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
     list_states = storage.all(State).values()
-    return render_template("7-states_list.html", states=list_states)
+    return render_template("8-cities_by_states.html", states=list_states)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
